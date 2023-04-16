3e98a578edad67c28b3705f8f783fc1147bcf013
khankul <khankul@riseup.net>
2023-04-17 00:04:39 +0700

    Set up directories and metadata files

---
+++ src/main/java/io/github/khankul/sandbox/mixin/TitleScreenMixin.java
@@ -0,0 +1,16 @@
+package io.github.khankul.sandbox.mixin;
+
+import io.github.khankul.sandbox.SandboxMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+	@Inject(method = "init", at = @At("TAIL"))
+	public void exampleMod$onInit(CallbackInfo ci) {
+		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
